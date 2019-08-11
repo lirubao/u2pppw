1946756f6ad51a9e3374a49868d000795b389103
lirubao <445266420@qq.com>
2019-08-12 02:19:13 +0800

    学习，源码仅供参考！

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lirubao/u2pppw
+
+go 1.12
---
+++ queue/queueentry/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lirubao/u2pppw/queue"
+)
+
+func main() {
+	q := queue.Queue{1}
+
+	q.Push(2)
+	q.Push(3)
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+}
